ab5d0bb9ee8bc7b27d58d83c0bd932134e64b9dd
Giovani Aguirre <me@its-g.io>
2019-10-21 18:55:41 -0700

    Added post method to easyHttp

--- easyHttp.js
+++ easyHttp.js
@@ -2,17 +2,30 @@ class EasyHttp {
   constructor() {
     this.xhr = new XMLHttpRequest;
   }
+
   get(url, limit, callback) {
     this.xhr.open("GET", url, true);
 
     this.xhr.onload = () => {
       let data = JSON.parse(this.xhr.responseText).splice(0, limit);
       if (this.xhr.status === 200) {
-        this.xhr.responseText
         callback(null, data);
+      } else {
+        callback(`Error: GET request fail`);
       }
     }
 
     this.xhr.send();
+  }
+
+  post(url, data, callback) {
+    this.xhr.open("POST", url, true);
+    this.xhr.setRequestHeader("Content-type", "application/json");
+
+    this.xhr.onload = () => {
+        callback(null, JSON.parse(this.xhr.responseText));
+    }
+
+    this.xhr.send(JSON.stringify(data));
   }
 }
--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 const http = new EasyHttp;
+const data = {
+    title: "This is the test title!",
+    body: "This is the test body!"
+}
 
-http.get("https://jsonplaceholder.typicode.com/posts/", 5, (err, res) => err ? console.log(err) : console.log(res));
+// GET
+// http.get("https://jsonplaceholder.typicode.com/posts/", 5, (err, res) => err ? console.log(err) : console.log(res));
+
+// POST
+http.post("https://jsonplaceholder.typicode.com/posts/", data, (err, res) => err ? console.log(err) : console.log(res));
