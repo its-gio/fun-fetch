20ea73d0489d666bca1cf75b67e65fa812d855ae
Giovani Aguirre <me@its-g.io>
2019-10-21 19:13:01 -0700

    Initial branch commit

--- scripts.js
+++ scripts.js
@@ -5,13 +5,9 @@ const data = {
 }
 
 // GET
-// http.get("https://jsonplaceholder.typicode.com/posts/", 5, (err, res) => err ? console.log(err) : console.log(res));
 
 // POST
-// http.post("https://jsonplaceholder.typicode.com/posts/", data, (err, res) => err ? console.log(err) : console.log(res));
 
 // PUT
-// http.put("https://jsonplaceholder.typicode.com/posts/", data, (err, res) => err ? console.log(err) : console.log(res), 4);
 
 // DELETE
-// http.delete("https://jsonplaceholder.typicode.com/posts/", (err, res) => err ? console.log(err) : console.log(res), 4);
